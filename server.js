1a0b04f3b3c1687c78bae10382d7823877fba8fe
=Diego Oliveira <diegosudolinux@gmail.com>
2025-05-20 19:28:05 -0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-    console.log(`Server is running, port ${PORT}`)
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
